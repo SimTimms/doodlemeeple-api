da6db4914746d70f32c9310a8a6faa4ffd20571f
Tim Simms <36262574+SimTimms@users.noreply.github.com>
2020-02-18 13:21:15 +0000

    Save

---
+++ api/initPassport.js
@@ -0,0 +1,27 @@
+import passport from 'passport';
+import { GraphQLLocalStrategy } from 'graphql-passport';
+
+const initPassport = ({ User }) => {
+  passport.use(
+    new GraphQLLocalStrategy((email, password, done) => {
+      const users = User.getUsers();
+      const matchingUser = users.find(
+        user => email === user.email && password === user.password,
+      );
+      const error = matchingUser ? null : new Error('no matching user');
+      done(error, matchingUser);
+    }),
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    const users = User.getUsers();
+    const matchingUser = users.find(user => user.id === id);
+    done(null, matchingUser);
+  });
+};
+
+export default initPassport;

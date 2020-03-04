30c8817bc3900381502e6699604889a8c58d1bd6
Tim Simms <36262574+SimTimms@users.noreply.github.com>
2020-03-04 14:16:11 +0000

    Fix

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -1,6 +1,10 @@
-export const UPDATED_PROFILE = {
+const UPDATED_PROFILE = {
   title: 'You updated a gallery',
   message: 'Make sure you showcase your best work',
   linkTo: '/app/edit-profile',
   icon: 'contact_mail',
+};
+
+module.exports = {
+  UPDATED_PROFILE,
 };

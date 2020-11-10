9db46cc07e73cb79f9af8ef1121d05a17bb4e52c
Tim Simms <36262574+SimTimms@users.noreply.github.com>
2020-11-10 11:36:59 +0000

    debug

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ app.post(
           const payment = await Payment.findOne({
             paymentId: event.data.object.payment_intent,
           });
-
+          console.log(contract);
           const contract = await Contract.findOne({ _id: payment.contract });
           await Contract.updateOne({ _id: contract._id }, { status: 'paid' });
           await Job.updateOne({ _id: contract.job }, { submitted: 'paid' });

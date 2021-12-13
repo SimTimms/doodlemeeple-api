ceec1b650a48915c209aa4583f6e5df434205164
Tim.Simms <tim-simms@hotmail.com>
2021-12-13 22:19:28 +0000

    Save

--- src/models/myPost.js
+++ src/models/myPost.js
@@ -1,7 +1,16 @@
 import mongoose, { Schema } from 'mongoose';
 import timestamps from 'mongoose-timestamp';
 import { composeWithMongoose } from 'graphql-compose-mongoose';
-import { UserTC, GameTC, ActivityLog, Kickstarter, User, Game, Job } from './';
+import {
+  UserTC,
+  GameTC,
+  JobTC,
+  ActivityLog,
+  Kickstarter,
+  User,
+  Game,
+  Job,
+} from './';
 import { getUserId } from '../utils';
 const ObjectId = mongoose.Types.ObjectId;
 
@@ -23,6 +32,10 @@ export const MyPostSchema = new Schema(
     game: {
       type: Schema.Types.ObjectId,
       ref: 'Game',
+    },
+    job: {
+      type: Schema.Types.ObjectId,
+      ref: 'Job',
     },
   },
   {
@@ -48,6 +61,14 @@ MyPostTC.addRelation('game', {
   resolver: () => GameTC.getResolver('findOne'),
   prepareArgs: {
     filter: (source) => ({ _id: ObjectId(source.game) }),
+  },
+  projection: { _id: true },
+});
+
+MyPostTC.addRelation('job', {
+  resolver: () => JobTC.getResolver('findOne'),
+  prepareArgs: {
+    filter: (source) => ({ _id: ObjectId(source.job) }),
   },
   projection: { _id: true },
 });
@@ -110,115 +131,133 @@ MyPostTC.addResolver({
 
 MyPostTC.addResolver({
   name: 'postFeed',
-  args: {},
+  args: { filter: ['String'] },
   type: [MyPostTC],
   kind: 'query',
   resolve: async (rp) => {
     const userId = getUserId(rp.context.headers.authorization);
 
     await User.updateOne({ _id: userId }, { lastOn: new Date() });
+    let myPosts = [];
+    if (rp.args.filter.indexOf('public') > -1) {
+      myPosts = await MyPost.find({
+        approved: true,
+      })
+        .sort({ createdAt: -1 })
+        .limit(15);
+    }
 
-    const myPosts = await MyPost.find({
-      approved: true,
-    })
-      .sort({ createdAt: -1 })
-      .limit(15);
+    let kickstarterArranged = [];
+    if (rp.args.filter.indexOf('kickstarter') > -1) {
+      const kickstarters = await Kickstarter.find({ approved: true })
+        .sort({ createdAt: -1 })
+        .limit(15);
 
-    const kickstarters = await Kickstarter.find({ approved: true })
-      .sort({ createdAt: -1 })
-      .limit(15);
+      kickstarterArranged = kickstarters.map((item) => {
+        return {
+          name: item.name,
+          summary: item.summary,
+          type: 'kickstarter',
+          createdAt: item.createdAt,
+          user: item.user._id,
+          url: item.url,
+          featuredImage: item.featuredImage,
+        };
+      });
+    }
 
-    const kickstarterArranged = kickstarters.map((item) => {
-      return {
-        name: item.name,
-        summary: item.summary,
-        type: 'kickstarter',
-        createdAt: item.createdAt,
-        user: item.user._id,
-        url: item.url,
-        featuredImage: item.featuredImage,
-      };
-    });
+    let jobsArranged = [];
+    if (rp.args.filter.indexOf('job') > -1) {
+      const jobs = await Job.find({ isPublic: true, approved: true })
+        .sort({ createdAt: -1 })
+        .limit(15);
 
-    const jobs = await Job.find({ isPublic: true, approved: true })
-      .sort({ createdAt: -1 })
-      .limit(15);
+      jobsArranged = jobs.map((item) => {
+        return {
+          job: item._id,
+          name: item.name,
+          summary: item.summary,
+          type: 'job',
+          createdAt: item.createdAt,
+          user: item.user._id,
+        };
+      });
+    }
 
-    const jobsArranged = jobs.map((item) => {
-      return {
-        name: item.name,
-        summary: item.summary,
-        type: 'job',
-        createdAt: item.createdAt,
-        user: item.user._id,
-      };
-    });
-
-    const users = await User.aggregate([
-      {
-        $match: {
-          $and: [
-            { profileImg: { $ne: '' } },
-            { profileImg: { $ne: null } },
-            { summary: { $ne: null } },
-            { summary: { $ne: '' } },
-            { sections: { $ne: [] } },
-            { sections: { $ne: null } },
-          ],
+    let usersArranged = [];
+    if (rp.args.filter.indexOf('newUser') > -1) {
+      const users = await User.aggregate([
+        {
+          $match: {
+            $and: [
+              { profileImg: { $ne: '' } },
+              { profileImg: { $ne: null } },
+              { summary: { $ne: null } },
+              { summary: { $ne: '' } },
+              { sections: { $ne: [] } },
+              { sections: { $ne: null } },
+            ],
+          },
         },
-      },
-      { $sort: { createdAt: -1 } },
-      { $limit: 12 },
-    ]);
-    const usersArranged = users.map((item) => {
-      return {
-        name: item.name,
-        summary: item.summary,
-        type: 'newUser',
-        createdAt: item.createdAt,
-        user: item._id,
-      };
-    });
+        { $sort: { createdAt: -1 } },
+        { $limit: 12 },
+      ]);
+      usersArranged = users.map((item) => {
+        return {
+          name: item.name,
+          summary: item.summary,
+          type: 'newUser',
+          createdAt: item.createdAt,
+          user: item._id,
+        };
+      });
+    }
 
-    const games = await Game.find().sort({ createdAt: -1 }).limit(15);
+    let gamesArranged = [];
+    if (rp.args.filter.indexOf('game') > -1) {
+      const games = await Game.find().sort({ createdAt: -1 }).limit(15);
 
-    const gamesArranged = games.map((item) => {
-      return {
-        game: item._id,
-        name: item.name,
-        summary: item.summary,
-        type: 'game',
-        createdAt: item.createdAt,
-        user: item.user._id,
-        featuredImage: item.featuredImage,
-      };
-    });
+      gamesArranged = games.map((item) => {
+        return {
+          game: item._id,
+          name: item.name,
+          summary: item.summary,
+          type: 'game',
+          createdAt: item.createdAt,
+          user: item.user._id,
+          featuredImage: item.featuredImage,
+        };
+      });
+    }
 
-    const usersSignedIn = await User.aggregate([
-      {
-        $match: {
-          $and: [
-            { profileImg: { $ne: '' } },
-            { profileImg: { $ne: null } },
-            { summary: { $ne: null } },
-            { summary: { $ne: '' } },
-            { sections: { $ne: [] } },
-            { sections: { $ne: null } },
-          ],
+    let usersSignedInArranged = [];
+    if (rp.args.filter.indexOf('lastOn') > -1) {
+      const usersSignedIn = await User.aggregate([
+        {
+          $match: {
+            $and: [
+              { profileImg: { $ne: '' } },
+              { profileImg: { $ne: null } },
+              { summary: { $ne: null } },
+              { summary: { $ne: '' } },
+              { sections: { $ne: [] } },
+              { sections: { $ne: null } },
+            ],
+          },
         },
-      },
-      { $sort: { lastOn: -1 } },
-      { $limit: 10 },
-    ]);
-    const usersSignedInArranged = usersSignedIn.map((item) => {
-      return {
-        name: item.name,
-        summary: item.summary,
-        type: 'lastOn',
-        createdAt: item.lastOn,
-        user: item._id,
-      };
-    });
+        { $sort: { lastOn: -1 } },
+        { $limit: 10 },
+      ]);
+      usersSignedInArranged = usersSignedIn.map((item) => {
+        return {
+          name: item.name,
+          summary: item.summary,
+          type: 'lastOn',
+          createdAt: item.lastOn,
+          user: item._id,
+        };
+      });
+    }
 
     const totalPosts = [
       ...kickstarterArranged,
